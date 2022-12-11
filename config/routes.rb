b1ae11be8b6aa927f0a6b399e0424f4acc37dbe2
dewble <dewble62@gmail.com>
2022-12-12 01:49:05 +0900

    Add 웹사이트 만들기 Update, Destroy 기능 추가

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get 'posts/show'
   # 상세 페이지
   get 'posts/show/:id' => "posts#show"
+  # 수정 페이지
+  get 'posts/edit/:id' => "posts#edit"
+  # update 하기
+  post 'posts/update/:id' => "posts#update"
+  # 게시글 삭제하기
+  get 'posts/destroy/:id' => "posts#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
